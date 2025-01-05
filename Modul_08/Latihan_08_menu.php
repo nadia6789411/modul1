f3faee9642a7f23dc1f88cacd1d67ea3100986b8
nadia6789411 <20230910073@uniku.ac.id>
2025-01-05 23:35:48 +0700

    Add files via upload

---
+++ Modul_08/Latihan_08_menu.php
@@ -0,0 +1,9 @@
+<aside class="col-md-2 py-0">
+    <nav class="nav flex-column bg-light p-3 m-0">
+        <a class="nav-link active" href="?menu=a">Latihan 8a</a>
+        <a class="nav-link active" href="?menu=b">Latihan 8b</a>
+        <a class="nav-link active" href="?menu=c">Latihan 8c</a>
+        <a class="nav-link active" href="?menu=d">Latihan 8d</a>
+        <a class="nav-link active" href="?menu=e">Latihan 8e</a
+    </nav>
+</aside>
